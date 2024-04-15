eafd3bc7e502c21794d57a968f727091ee1b9611
yehudise <yehudise02@gmail.com>
2024-04-14 21:00:04 -0500

    finally got it

--- calc/app.py
+++ calc/app.py
@@ -1,2 +1,36 @@
-#hello
- Put your app in here.
+"""Put your app in here."""
+from flask import Flask,request
+from operations import add,sub,mult,div
+app = Flask(__name__)
+
+@app.route("/add", methods=['GET'])
+def do_add():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a,b)
+    return str(result)
+
+@app.route("/sub", methods=['GET'])
+def do_sub():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a,b)
+    return str(result)
+
+@app.route("/mult", methods=['GET'])
+def do_mult():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a,b)
+    return str(result)
+
+@app.route("/div", methods=['GET'])
+def do_div():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a,b)
+    return str(result)
+
+
+
+
